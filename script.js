7ebfbb45a521fd355b93aa0f6c4df09cc71d3860
ole-kristian-rudjord <s354564@oslomet.no>
2023-01-22 18:40:26 +0100

    Added download button

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-/* window.print(); */
-window.addEventListener("click", function() {
-  this.print();
-});
+window.onload = function() {
+
+  let btn = document.getElementById("print-btn");
+  let div = document.getElementById("print-div");
+
+  btn.addEventListener("click", () => {
+    div.removeChild(btn);
+    print();
+    div.appendChild(btn);
+  });
+};
